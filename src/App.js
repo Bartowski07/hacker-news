046febef0bc19a6567f5dbcce1a3c77d75331852
rahgawirajakumaraswamy <rahgawiraja1999@outlook.de>
2023-04-20 11:25:19 +0200

    created hnAPi.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import {getStoryIds} from './services/hnApi';
 
 
-export const App =() => <p>Hello</p>
+export const App = () => {
+ const [storyIds,setStoryIds] = useState([]);
+    
+  useEffect(() => {
+    setStoryIds('hello');
+
+  },[]);
+   
+    return <p>{storyIds}</p>
+   
+};
   
 
 
